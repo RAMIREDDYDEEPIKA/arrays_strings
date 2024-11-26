0d0dafac7cef5504f19363ffcad852085361e2ea
RAMIREDDYDEEPIKA <151945678+RAMIREDDYDEEPIKA@users.noreply.github.com>
2024-11-26 15:03:07 +0530

    checking the given string is palindrome or not

--- PalindromeCheck.java
+++ PalindromeCheck.java
@@ -1,19 +1,22 @@
 package com.arrays;
 
-public class PalindromeCheck {
-
-	public static void main(String[] args) {
+public class PalindromeCheck 
+{
+	public static void main(String[] args) 
+	{
 		 String str="madam";
 		 String result="";
-		 for(int i=str.length()-1;i>=0;i--) {
-		      result=result+str.charAt(i);
+		 for(int i=str.length()-1;i>=0;i--) 
+		 {
+		         result=result+str.charAt(i);
 		 }
-		 if(str.equalsIgnoreCase(result)) {
-			 System.out.println(result+" is palindrome");
+		 if(str.equalsIgnoreCase(result)) 
+		 {
+			System.out.println(result+" is palindrome");
 		 }
-		 else {
-			 System.out.println(str+" is not palindrome");
+		 else 
+		 {
+			System.out.println(str+" is not palindrome");
 		 }
 	}
-
 }
