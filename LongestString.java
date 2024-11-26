1e992cf89ddaf395a9307d534cb321ff2e702e83
RAMIREDDYDEEPIKA <151945678+RAMIREDDYDEEPIKA@users.noreply.github.com>
2024-11-26 14:07:30 +0530

    Finding the longest string

--- LongestString.java
+++ LongestString.java
@@ -6,19 +6,20 @@ public class LongestString
 	public static void main(String[] args) 
 	{
 		String input = "abcabcb";
-        int maxLength = 0;
-        int duplicate = 0;
-        HashSet<Character> subString = new HashSet<>();
-        for (int i = 0; i < input.length(); i++) 
-        {
-            char currentChar = input.charAt(i);
-            while (subString.contains(currentChar)) {
-                subString.remove(input.charAt(duplicate));
-                duplicate++;
-            } 
-            subString.add(currentChar);
-            maxLength = Math.max(maxLength, i - duplicate + 1);
-        }
-        System.out.println("The substring length are: "+maxLength);		
+        	int maxLength = 0;
+        	int duplicate = 0;
+       		HashSet<Character> subString = new HashSet<>();
+        	for (int i = 0; i < input.length(); i++) 
+        	{
+            		char currentChar = input.charAt(i);
+            		while (subString.contains(currentChar)) 
+			{
+                		subString.remove(input.charAt(duplicate));
+                		duplicate++;
+            		} 
+            		subString.add(currentChar);
+           		maxLength = Math.max(maxLength, i - duplicate + 1);
+        	}
+        	System.out.println("The substring length are: "+maxLength);		
 	}
 }
