f7bd77b3731048871c3149c5c00798557fbeabdf
RAMIREDDYDEEPIKA <151945678+RAMIREDDYDEEPIKA@users.noreply.github.com>
2024-11-26 13:56:27 +0530

    matrix multiplication solution

--- MatrixMultiplication.java
+++ MatrixMultiplication.java
@@ -1,20 +1,25 @@
 package com.arrays;
 
-public class MatrixMultiplication {
-
-	public static void main(String[] args) {
+public class MatrixMultiplication 
+{
+	public static void main(String[] args) 
+	{
 		 int[][] arr= {{1,2,3},{2,4,5},{2,5,7}};
 		 int[][] arr2= {{2,3,5},{1,5,3},{6,7,5}};
-		 if(arr.length!=arr2.length || arr[0].length!=arr2[0].length) {
+		 if(arr.length!=arr2.length || arr[0].length!=arr2[0].length) 
+		 {
 			 System.out.println("Check once! matrix must be same length or not");
 		 }
 		 int[][] matrixMul=new int[arr.length][arr[0].length];
-		 for(int i=0;i<arr.length;i++) {
-			 for(int j=0;j<arr[0].length;j++) {
+		 for(int i=0;i<arr.length;i++) 
+		 {
+			 for(int j=0;j<arr[0].length;j++) 
+			 {
 				 matrixMul[i][j]=arr[i][j] * arr2[i][j];
 			 } 
 		 } 
-		 for(int i=0;i<matrixMul.length;i++) {
+		 for(int i=0;i<matrixMul.length;i++) 
+		 {
 			 for(int j=0;j<matrixMul.length;j++)
 			 {
 				 System.out.print(matrixMul[i][j]+" ");
@@ -22,5 +27,4 @@ public class MatrixMultiplication {
 			 System.out.println();
 		 }
 	}
-
 }
