acf59eceae9249e406aaf5a69c39b63de3ae2fab
RAMIREDDYDEEPIKA <151945678+RAMIREDDYDEEPIKA@users.noreply.github.com>
2024-11-26 15:05:02 +0530

    Checking the String is anagram or not

--- StringAnagram.java
+++ StringAnagram.java
@@ -2,26 +2,30 @@ package com.arrays;
 
 import java.util.Arrays;
 
-public class StringAnagram {
-
-	public static void main(String[] args) {
+public class StringAnagram 
+{
+	public static void main(String[] args)
+	{
 		 String word="listen";
 		 String result="silent";
-		 if(word.length() != result.length()) {
+		 if(word.length() != result.length()) 
+		 {
 			 System.out.println("Given two Strings must be same length to chack it is anagram or not");
 		 }
-		 else {
+		 else 
+		 {
 			 char[] charArray1=word.toCharArray();
 			 char[] charArray2=result.toCharArray();
 			 Arrays.sort(charArray1);
 			 Arrays.sort(charArray2);
-			 if(Arrays.equals(charArray1, charArray2)) {
+			 if(Arrays.equals(charArray1, charArray2)) 
+			 {
 				 System.out.println(word+" is anagram string");
 			 }
-			 else {
+			 else 
+			 {
 				 System.out.println(word+" is not Anagram string");
 			 }
 		 }
 	}
-
 }
