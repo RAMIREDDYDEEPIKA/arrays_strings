cc3ddd528553b261b885fce409bda2640d51e998
RAMIREDDYDEEPIKA <151945678+RAMIREDDYDEEPIKA@users.noreply.github.com>
2024-11-26 13:53:35 +0530

    Solutions for array problems

--- CountEvenOdd.java
+++ CountEvenOdd.java
@@ -1,25 +1,28 @@
 package com.arrays;
 
-public class CountEvenOdd {
-
-	public static void main(String[] args) {
+public class CountEvenOdd
+{
+	public static void main(String[] args)
+	{
 		 int[] arr= {1,2,3,4,5,6,7,8};
 		 int evenCount=0,oddCount=0;
-		 for(int i=0;i<arr.length;i++) {
-			 if(arr[i]%2==0) {
+		 for(int i=0;i<arr.length;i++)
+		 {
+			 if(arr[i]%2==0) 
+			 {
 				 System.out.println(arr[i]+" ");
 				 evenCount++;
 			 }
-		 }System.out.println("Even elements present in the array are: "+evenCount);
-		 for(int i=0;i<arr.length;i++) {
-			 if(arr[i]%2!=0) {
+		 }
+		 System.out.println("Even elements present in the array are: "+evenCount);
+		 for(int i=0;i<arr.length;i++)
+		 {
+			 if(arr[i]%2!=0)
+			 {
 				 System.out.println(arr[i]+" ");
 				 oddCount++;
 			 }
 		 }
 		 System.out.println("Odd elements count: "+oddCount);
-		 
-
 	}
-
 }
